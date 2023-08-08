635d7408fc8febade987bd44a9da7190ed664642
masao fujihara <masao_fujihara@yahoo.co.jp>
2023-08-09 00:08:00 +0900

    1st commit

--- RockCissersPaper.py
+++
@@ -1,9 +0,0 @@
-# 基本ライブラリ
-import streamlit as st
-import numpy as np
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
-
-# データセットの読み込み
-hand_pattern = new_df()
-df = pd.DataFrame(iris.data, columns=iris.feature_names)
--- main.py
+++ main.py
@@ -1,37 +1,82 @@
+###選択肢でじゃんけん 完成　Streamlitにあげる
+
 #必要なライブラリをインポート
 import streamlit as st
-import numpy as np
 from PIL import Image
 import time
 import random
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-import xgboost
 from xgboost import XGBClassifier
-from sklearn.tree import DecisionTreeClassifier
+
+# データセットの読み込み
+datas = 'C:\\Users\\masao\\Desktop\\Pythonフォルダ\\guchokipa_Select\\DataFrame.csv'
+df = pd.read_csv(datas)
+
+# 目標値（目的変数）の指定
+target_column = 'target'  # 目標値の列名を適宜変更する必要があります
+y = df[target_column]
+x = df.drop(target_column, axis=1)  # 目標値を除いた特徴量をXとして使用
+
+# 予測モデルの構築
+model = XGBClassifier(booster="gbtree",             # ブースター種類（ツリーモデル：gbtree or dart, 線形モデル：gblinear)
+                        learning_rate=1,              # 過学習防止を目的とした学習率
+                        min_split_loss=0,             # 決定木の葉ノード追加に伴う損失減少の下限値
+                        max_depth=6,                  # 決定木の深さの最大値
+                        min_child_weight=1,           # 決定木の葉に必要な重みの下限
+                        subsample=1,                  # 各決定木においてランダム抽出されるサンプル割合
+                        sampling_method="uniform",    # サンプリング方法
+                        colsample_bytree=1,           # 各決定木でランダムに設定される説明変数の割合
+                        colsample_bylevel=1,          # 決定木が深さ単位で分割される際に利用する説明変数の割合
+                        reg_lambda=1,                 # L2正則化のペナルティ項
+                        reg_alpha=0,                  # L1正則化のペナルティ項
+                        tree_method="auto",           # ツリー構造アルゴリズム
+                        process_type="default",       # 実行するブースティングプロセス
+                        grow_policy="depthwise",      # 新しい葉ノードを木に追加する際の制御ポリシー
+                        max_leaves=0,                 # 追加する葉ノードの最大数
+                        objective="reg:squarederror", # 学習プロセスで最小化を目指す損失関数
+                        num_round=9,                  # ブースティング回数(=作成する決定木の本数)
+                        )
+model.fit(x, y)
+
+# ダメだったモデル RandomForestRegressor / GradientBoostingRegressor
+
 
 #　タイトルとテキストを記入
 st.title('AI じゃん・けん・ぽん')
 
-# トップ画像を読み込み、表示する
-# image3 = Image.open('ぐーちょきぱー.png')
-# st.image(image3)
-
-
-
 # それぞれの手の画像を読み込む
+# COM と ユーザー それぞれ45度かたむける
 image0 = Image.open('ぐー.png')
+im_rotate0_com = image0.rotate(45, expand=True) #COM用
+im_rotate0_user = image0.rotate(-45, expand=True) #ユーザー用
 image1 = Image.open('ちょき.png')
+im_rotate1_com = image1.rotate(45, expand=True) #COM用
+im_rotate1_user = image1.rotate(-45, expand=True) #ユーザー用
 image2 = Image.open('ぱー.png')
-choices = ['ぐー', 'ちょき', 'ぱー']
-choices_mapping = {'ぐー': 0, 'ちょき': 1, 'ぱー': 2}
+im_rotate2_com = image2.rotate(45, expand=True) #COM用
+im_rotate2_user = image2.rotate(-45, expand=True) #ユーザー用
+
+# 変数に代入する
+choices = ['ぐー　', 'ちょき', 'ぱー　']
+choices_mapping = {0: 'ぐー', 1: 'ちょき', 2: 'ぱー', 3:'-'}
+# 対戦結果のグーチョキパーそれぞれの加算数値を 0 に初期化する
 result0 = 0
 result1 = 0
 result2 = 0
+hand = 0
+win_lose = 0
+if "n" not in st.session_state:
+    st.session_state.n = 3
+if "X1" not in st.session_state:
+    st.session_state.X1 = 3
+if "X2" not in st.session_state:
+    st.session_state.X2 = 3
+if "X3" not in st.session_state:
+    st.session_state.X3 = 3
 
 
 # 以下サイドバーの内容
-# どの手を出すか選択するラジオボタンを表示
+# どの手を出すか選択するボタンを表示
 with st.sidebar:
 
     st.header('出す手を選んでください')
@@ -45,10 +90,12 @@ with st.sidebar:
 
     st.header('対戦回数')
     # ボタンのクリック回数をカウントする変数
+    # session_state は過去の数値を引き継いで加算する
     if "click_count" not in st.session_state:
         st.session_state.click_count = 0
         st.write(f'{st.session_state.click_count} 回')
-    
+
+    # ３つのボタンどれかをクリックすると回数を加算する
     if clicked1 or clicked2 or clicked3:
         st.session_state.click_count += 1
         # ボタンのクリック回数の表示
@@ -56,6 +103,7 @@ with st.sidebar:
 
 
     st.header('対戦成績')
+    # 対戦成績の回数をセット
     win_col, lose_col, eql_col = st.columns(3)
     if "win" not in st.session_state:
         st.session_state.win = 0
@@ -67,102 +115,109 @@ with st.sidebar:
     # データフレームで対戦ログを表示
     st.write('※ぐー: 0　ちょき: 1　ぱー: 2')
     if "hand_log" not in st.session_state:
-        st.session_state.hand_log = pd.DataFrame(columns=['あなた', 'COM', '結果'])
-    
-#    col0, col1, col2 = st.columns(3)
         df_new = pd.DataFrame(columns=['あなた', 'COM', '結果'])
-        #st.write(df_new)
+        st.session_state.hand_log = df_new
+        st.write(df_new)
 
 # COMが出す手をランダムで決める
-if st.session_state.click_count < 6:
-    n = random.randint(0, 2)
+if st.session_state.click_count < 4:
+    st.session_state.n = random.randint(0, 2)
     print('random')
-else:
+    print(st.session_state.n)
+#else:
     # 学習用のデータとして手の履歴を取得
-    df_new = st.session_state.hand_log
+    #df_new = st.session_state.hand_log
     #print(df_new)
-    if len(df_new) > 0:
-        x = df_new['あなた'].values[:-1]
-        x = x.reshape(-1, 1)
-        t = df_new['COM'].values[1:]
-        t = t.astype(int)
-        model = DecisionTreeClassifier()
-        # model = RandomForestClassifier() #決定木でもいい
-        #model = XGBClassifier()
-        model.fit(x, t)
-        n = model.predict([[df_new.iloc[-1]['あなた']]])
-        if n == 0:
-            n = 2
-        elif n == 1:
-            n = 0
-        else:
-            n = 1
-        print('gx')
+    #if len(df_new) > 0:
+        #X_Value = pd.DataFrame([[st.session_state.X3, st.session_state.X2, st.session_state.X1]])
+        # 予測値のデータフレーム
+        #n = model.predict_proba(X_Value)
+        #print(X_Value)
+        #print(n)
+        # 最大要素のインデックスを取得
+        #n = np.argmax(n)
+        #print(n)
+        #print('model')
+
 
 # 手のボタン をクリックすると、イベント開始
 if clicked1 or clicked2 or clicked3:
-
     # じゃん・けん・ぽん を順番に表示
-    col1, col2, col3 = st.columns(3)
-    with col1:
+    jyan_col1, ken_col2, pon_col3 = st.columns(3)
+    with jyan_col1:
         time.sleep(0.5)
         st.subheader('じゃん')
-    with col2:
+    with ken_col2:
         time.sleep(0.5)
         st.subheader('けん')
-    with col3:
+    with pon_col3:
         time.sleep(0.5)
         st.subheader('ぽん')
 
-    # coa1(選んだユーザーの手)、col2(ランダムのCOMの手)を表示させる配置を設定'
-    col1, col2 = st.columns(2)
+    # coa2(選んだユーザーの手)、col3(ランダムのCOMの手)を表示させる配置を設定'
+    boy_col1, user_col2, com_col3, robot_col4 = st.columns(4)
 
     # ユーザーの手の画像を表示
-    with col1:
+    st.session_state.X3 = st.session_state.X2
+    st.session_state.X2 = st.session_state.X1
+    
+    with boy_col1:
+        boy = Image.open('男の子.png')
+        st.image(boy, width=200)
+
+    with user_col2:
         if clicked1:
-            st.image(image0, width=250)
+            st.image(im_rotate0_user, width=220)
             hand = 0
+            st.session_state.X1 = 0
         elif clicked2:
-            st.image(image1, width=200)
+            st.image(im_rotate1_user, width=200)
             hand = 1
+            st.session_state.X1 = 1
         else:
-            st.image(image2, width=240)
+            st.image(im_rotate2_user, width=200)
             hand = 2
+            st.session_state.X1 = 2
 
     # COMの手の画像を表示
-    with col2:
-        if n == 0:
-            st.image(image0, width=250)
-        elif n == 1:
-            st.image(image1, width=200)
+    with com_col3:
+        if st.session_state.n == 0:
+            st.image(im_rotate0_com, width=220)
+        elif st.session_state.n == 1:
+            st.image(im_rotate1_com, width=200)
         else:
-            st.image(image2, width=240)
+            st.image(im_rotate2_com, width=200)
 
-    if hand == 0 and n == 0: #ぐーvsぐー
+    with robot_col4:
+        robot = Image.open('ロボット.png')
+        st.image(robot, width=200)
+
+
+    if hand == 0 and st.session_state.n == 0: #ぐーvsぐー
         st.header('あいこ')
         result2 = 1
-    elif hand == 0 and n == 1: #ぐーvsちょき
+    elif hand == 0 and st.session_state.n == 1: #ぐーvsちょき
         st.header('あなたの 勝ち')
         result0 = 1
-    elif hand == 0 and n == 2: #ぐーvsぱー
+    elif hand == 0 and st.session_state.n == 2: #ぐーvsぱー
         st.header('あなたの 負け')
         result1 = 1
-    elif hand == 1 and n == 0: #ちょきvsぐー
+    elif hand == 1 and st.session_state.n == 0: #ちょきvsぐー
         st.header('あなたの 負け')
         result1 = 1
-    elif hand == 1 and n == 1: #ちょきvsちょき
+    elif hand == 1 and st.session_state.n == 1: #ちょきvsちょき
         st.header('あいこ')
         result2 = 1
-    elif hand == 1 and n == 2: #ちょきvsぱー
+    elif hand == 1 and st.session_state.n == 2: #ちょきvsぱー
         st.header('あなたの 勝ち')
         result0 = 1
-    elif hand == 2 and n == 0: #ぱーvsぐー
+    elif hand == 2 and st.session_state.n == 0: #ぱーvsぐー
         st.header('あなたの 勝ち')
         result0 = 1
-    elif hand == 2 and n == 1: #ぱーvsちょき
+    elif hand == 2 and st.session_state.n == 1: #ぱーvsちょき
         st.header('あなたの 負け')
         result1 = 1
-    elif hand == 2 and n == 2: #ぱーvsぱー
+    elif hand == 2 and st.session_state.n == 2: #ぱーvsぱー
         st.header('あいこ')
         result2 = 1
 
@@ -170,27 +225,44 @@ if clicked1 or clicked2 or clicked3:
     st.session_state.lose += result1
     st.session_state.eql += result2
 
-    with win_col:
-        st.write(f'勝ち {st.session_state.win} 回')
-    with lose_col:
-        st.write(f'負け {st.session_state.lose} 回')
-    with eql_col:
-        st.write(f'あいこ {st.session_state.eql} 回')
-
-    # COMが勝つ手を計算
-    #if hand == 0:
-    #    win_hand = 2
-    #elif hand == 1:
-    #    win_hand = 0
-    #else:
-    #    win_hand = 1
     if result0 == 1:
         win_lose = "〇"
     elif result1 == 1:
         win_lose = "×"
-    else:
+    elif result2 == 1:
         win_lose = "△"
 
-    df_new = pd.DataFrame({'あなた': [hand], 'COM': [n], '結果': [win_lose]})
+    #st.write('2回前:', choices_mapping[st.session_state['X2']])
+    df_new = pd.DataFrame({'あなた': choices_mapping[hand], 'COM': choices_mapping[st.session_state.n], '結果': [win_lose]})
     st.session_state.hand_log = pd.concat([st.session_state.hand_log, df_new], axis=0).reset_index(drop=True)
     st.sidebar.dataframe(st.session_state.hand_log)
+
+
+    log_col1, pred_col2 = st.columns(2)
+    with log_col1:
+        st.write('1回前:', choices_mapping[st.session_state['X1']])
+        st.write('2回前:', choices_mapping[st.session_state['X2']])
+        st.write('3回前:', choices_mapping[st.session_state['X3']])
+
+    with pred_col2:
+        # データフレームを作成
+        X_Value = pd.DataFrame([[st.session_state.X3, st.session_state.X2, st.session_state.X1]], columns=['X3', 'X2', 'X1'])
+        # 予測値のデータフレーム
+#        st.session_state.n = model.predict_proba(X_Value)
+        st.session_state.n = model.predict(X_Value)
+        print(X_Value)
+        print(st.session_state.n)
+        st.session_state.n = st.session_state.n.item()
+        print(st.session_state.n)
+        if st.session_state.click_count > 2:
+            st.write('robot が次に出そうとしている手:', choices_mapping[st.session_state['n']])
+
+
+
+with win_col:
+    st.write(f'勝ち {st.session_state.win} 回')
+with lose_col:
+    st.write(f'負け {st.session_state.lose} 回')
+with eql_col:
+    st.write(f'あいこ {st.session_state.eql} 回')
+
